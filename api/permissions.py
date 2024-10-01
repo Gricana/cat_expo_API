b1d5f82ac5476805fe98ad633bfbf364cd6af13b
Gricana <alex2904.goryachev@yandex.com>
2024-10-01 21:43:43 +0300

    Permission error message overridden

--- api/permissions.py
+++ api/permissions.py
@@ -1,10 +1,14 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS or (
+                obj.owner == request.user):
             return True
 
-        return obj.owner == request.user
+        raise PermissionDenied({
+            "detail": "Отстаньте от чужого кота )"
+        })

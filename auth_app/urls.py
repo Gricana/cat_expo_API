f3929a10f1957b8c73de07fb42cf08128870f845
Gricana <alex2904.goryachev@yandex.com>
2024-10-01 21:36:09 +0300

    Route configured

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,5 +6,5 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

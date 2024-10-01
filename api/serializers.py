107f667eb2be558f9d75607d13ee19563a885d7f
Gricana <alex2904.goryachev@yandex.com>
2024-10-01 21:42:13 +0300

    The order of creating / changing cat records has been redefined

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from django.db.models import Avg
 from rest_framework import serializers
 
-from .models import Breed, Cat, Rating
+from .models import Breed, Cat, Rating, Color
 
 
 class BreedSerializer(serializers.ModelSerializer):
@@ -19,9 +19,14 @@ class RatingSerializer(serializers.ModelSerializer):
 
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    average_rating = serializers.SerializerMethodField()
-    ratings = RatingSerializer(many=True, read_only=True)
+    breed = serializers.CharField(max_length=100, help_text="Порода кошки")
+    color = serializers.CharField(max_length=30, help_text="Цвет кошки")
+    owner = serializers.ReadOnlyField(source='owner.username',
+                                      help_text="Владелец кошки")
+    average_rating = serializers.SerializerMethodField(
+        help_text="Средний рейтинг")
+    ratings = RatingSerializer(many=True, read_only=True,
+                               help_text="Оценки пользователей")
 
     class Meta:
         model = Cat
@@ -33,16 +38,51 @@ class CatSerializer(serializers.ModelSerializer):
             return round(ratings.aggregate(Avg('rating'))['rating__avg'], 2)
         return None
 
+    def create(self, validated_data):
+        breed_name = validated_data.pop('breed')
+        color_name = validated_data.pop('color')
+
+        breed, created = Breed.objects.get_or_create(name=breed_name)
+        color, created = Color.objects.get_or_create(name=color_name)
+
+        validated_data['breed'] = breed
+        validated_data['color'] = color
+
+        return Cat.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        if ('breed', 'color') in validated_data:
+            breed_name = validated_data.pop('breed')
+            color_name = validated_data.pop('color')
+
+            breed, created = Breed.objects.get_or_create(name=breed_name)
+            color, created = Color.objects.get_or_create(name=color_name)
+
+            instance.breed = breed
+            instance.color = color
+
+        return super().update(instance, validated_data)
+
 
 class CatRatingSerializer(serializers.ModelSerializer):
+    cat = CatSerializer(many=False, read_only=True)
+
     class Meta:
         model = Rating
         fields = ('cat', 'rating',)
 
     def validate(self, data):
-        if data['cat'].owner == self.context['request'].user:
+        cat = self.context['cat']
+        user = self.context['request'].user
+
+        if self.context['cat'].owner == self.context['request'].user:
             raise serializers.ValidationError('Нельзя оценивать своего '
                                               'собственного котёнка <3')
+
+        if Rating.objects.filter(cat=cat, user=user).exists():
+            raise serializers.ValidationError(
+            "Вижу, что ты меня переоцениваешь. meow...")
+
         return data
 
     def create(self, validated_data):

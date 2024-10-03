0cc99806eae8cff607f3333df59aacc5d8cdcaa2
Gricana <alex2904.goryachev@yandex.com>
2024-10-03 15:17:19 +0300

    Documented serializer to handle authentication

--- auth_app/serializers.py
+++ auth_app/serializers.py
@@ -3,12 +3,25 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели пользователя.
+    """
     password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> User:
+        """
+        Метод для создания нового пользователя.
+        Пароль автоматически хэшируется.
+
+        Аргументы:
+            validated_data (dict): Валидированные данные.
+
+        Возвращает:
+            User: Созданный экземпляр пользователя.
+        """
         user = User.objects.create_user(**validated_data)
         return user

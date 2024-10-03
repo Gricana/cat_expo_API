8a54f63c101f5b6dc087b335a4a3dac7bdfda3a8
Gricana <alex2904.goryachev@yandex.com>
2024-10-03 14:49:54 +0300

    Refactored and coded owner permission code

--- api/permissions.py
+++ api/permissions.py
@@ -3,10 +3,23 @@ from rest_framework.exceptions import PermissionDenied
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Пользовательское разрешение, позволяющее редактировать объект
+    только его владельцу. Остальные пользователи имеют доступ только для чтения.
+    """
 
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS or (
-                obj.owner == request.user):
+    def has_object_permission(self, request, view, obj) -> bool:
+        """
+        Проверяет права на доступ к объекту.
+        Доступ для чтения предоставляется всем пользователям.
+        Изменять объект может только владелец.
+
+        :param request: Запрос пользователя.
+        :param view: Представление (View), к которому запрашивается доступ.
+        :param obj: Объект, к которому запрашивается доступ.
+        :return: True, если доступ разрешён, иначе PermissionDenied.
+        """
+        if request.method in permissions.SAFE_METHODS or obj.owner == request.user:
             return True
 
         raise PermissionDenied({
